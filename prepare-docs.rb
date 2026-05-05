c73700180bb1ae5b876e727834c6330780c1835f
Jimmy Tanagra <jcode@tanagra.id.au>
2026-05-06 00:08:13 +1000

    add yaml docs subdirectory
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- prepare-docs.rb
+++ prepare-docs.rb
@@ -323,6 +323,15 @@ end
 verbose "   ➡️ images"
 FileUtils.cp_r(".vuepress/openhab-docs/configuration/images", "docs/configuration")
 
+puts "➡️ Migrating the YAML Configuration section"
+Dir.glob(".vuepress/openhab-docs/configuration/yaml/*.md") do |path|
+  file = File.basename(path)
+
+  verbose "   ➡️ #{file}"
+  process_file(".vuepress/openhab-docs/configuration/yaml", file, "docs/configuration/yaml",
+               "#{$docs_repo_root}/configuration/yaml/#{file}")
+end
+
 puts "➡️ Migrating the Main UI section"
 Dir.glob(".vuepress/openhab-docs/mainui/*.md") do |path|
   file = File.basename(path)
